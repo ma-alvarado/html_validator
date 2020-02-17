193ffd326da6ea71a5b037962d388a1bf9222deb
Matias <malvarado22@cmc.edu>
2020-02-16 20:30:07 -0800

    modified code for balanced parenthesis

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -4,13 +4,28 @@
 def validate_html(html):
     '''
     This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
-
+top[2:]top[2:]
     >>> validate_html('<strong>example</strong>')
     True
     >>> validate_html('<strong>example')
     False
     '''
-
+    balanced = True
+    mylist = []
+    for tag in _extract_tags(html):
+        if "/" not in tag:
+            mylist.append(tag)
+        else:
+            if mylist == []:
+                return False
+            else:
+                top = mylist.pop()
+                if top[2:] not in tag[1:]:
+                    balanced = False
+    if balanced and mylist == []:
+        return True
+    else:
+        return False
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
     # then process these html tags using the balanced parentheses algorithm from the book
@@ -29,3 +44,14 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    tags = []
+    while len(html) != 0:
+        lefttag = html.find("<")
+        righttag = html.find(">")
+
+        if lefttag==-1 or righttag==-1:
+            break
+        
+        tags.append(html[lefttag:righttag+1])
+        html = html[(righttag+1):]
+    return tags
